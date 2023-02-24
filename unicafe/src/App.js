d1aa8dc208f18ec316ec006ee4ba42bcb9d70d62
Saranga <54878558+Saranaga123@users.noreply.github.com>
2023-02-24 14:42:01 +0530

    Final unicafe
    Final unicafe

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,37 +1,37 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const total = good + neutral + bad;
+  const average = (good - bad) / total || 0;
+  const positive = (good / total) * 100 || 0;
 
   return (
     <div>
-      <h1>{course.name}</h1>
-      <p>
-        {course.parts[0].name} {course.parts[0].exercises}
-      </p>
-      <p>
-        {course.parts[1].name} {course.parts[1].exercises}
-      </p>
-      <p>
-        {course.parts[2].name} {course.parts[2].exercises}
-      </p>
-      <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <h2>Statistics</h2>
+      {total === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <div>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>Total: {total}</p>
+          <p>Average: {average}</p>
+          <p>Positive: {positive} %</p>
+        </div>
+      )}
     </div>
   )
 }
+
 
 export default App
